47e81702596998b3f20332a7118094d19bc8df1a
15488 <mads.gadeberg@nordakademie.de>
2018-12-05 15:28:42 +0100

    Impleneted customer list including update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {DroneDetailsComponent} from "./drone/drone-details/drone-details.compone
 import {ManufacturerDetailsComponent} from "./manufacturer/manufacturer-details/manufacturer-details.component";
 import {HomeComponent} from './home/home.component';
 import {CustomerDetailsComponent} from "./customer/customer-details/customer-details.component";
+import {CustomerListComponent} from "./customer/customer-list/customer-list.component";
 
 const routes: Routes = [
   { path: 'drones', component: DroneListComponent},
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'manufacturers/:id', component: ManufacturerDetailsComponent},
   { path: 'drones', component: DroneListComponent},
   { path: '', component: HomeComponent},
-  { path: 'customers/:id', component: CustomerDetailsComponent}
+  { path: 'customers/:id', component: CustomerDetailsComponent},
+  { path: 'customers', component: CustomerListComponent}
 ];
 
 @NgModule({
---
+++ src/app/customer/customer-list/customer-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {CustomerService} from "../../shared/services/customer.service";
+import {Customer} from "../../shared/model/customer";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.css']
+})
+export class CustomerListComponent implements OnInit {
+
+  customers: Customer[];
+  isUpdate = false;
+  customer: Customer;
+
+
+  customerForm = new FormGroup({
+    id: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    email: new FormControl('')
+  });
+  constructor(private route: ActivatedRoute, private customerService: CustomerService, private router: Router) { }
+
+
+  ngOnInit() {
+    this.customerService.getCustomers().subscribe(customers => this.customers = customers);
+  }
+
+  setUpdate(customerUpdate: Customer){
+    this.isUpdate = true;
+      this.customerForm.patchValue({
+        id: customerUpdate.id,
+        firstName: customerUpdate.firstName,
+        lastName: customerUpdate.lastName,
+        address: customerUpdate.address,
+        phoneNumber: customerUpdate.phoneNumber,
+        email: customerUpdate.email
+      });
+    }
+
+  resetUpdate(){
+    this.isUpdate = false;
+  }
+
+  update() {
+    const customer = this.customerForm.value;
+    this.customerService.updateCustomer(customer)
+      .subscribe(() => {
+        this.router.navigateByUrl('/customers');
+      });
+  }
+
+}
