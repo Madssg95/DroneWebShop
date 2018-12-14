21a0f59b03c364fca71dc7815a88cc4dd256f042
kENT <pedersenkentjuul@gmail.com>
2018-12-14 11:32:06 +0100

    fixins

--- src/app/shared/services/packages.service.ts
+++ src/app/shared/services/packages.service.ts
@@ -1,5 +1,5 @@
 import {HttpClient} from '@angular/common/http';
-import {packages} from '../model/packages';
+import {Packages} from '../model/packages';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {Injectable} from '@angular/core';
@@ -11,20 +11,20 @@ export class packagesService{
 
   constructor(public http: HttpClient) { }
 
-  getPackages(): Observable<packages[]> {
-    return this.http.get<packages[]>(environment.apiUrl + 'package');
+  getPackages(): Observable<Packages[]> {
+    return this.http.get<Packages[]>(environment.apiUrl + 'package');
   }
 
-  getPackageById(id: number): Observable<packages> {
-    return this.http.get<packages>(environment.apiUrl + 'package/' + id);
+  getPackageById(id: number): Observable<Packages> {
+    return this.http.get<Packages>(environment.apiUrl + 'package/' + id);
   }
 
-  createPackage(packages: packages): Observable<packages> {
-    return this.http.post<packages>(environment.apiUrl + 'package', packages);
+  createPackage(packages: Packages): Observable<Packages> {
+    return this.http.post<Packages>(environment.apiUrl + 'package', packages);
   }
 
-  updatePackage(packages: packages): Observable<packages> {
-    return this.http.put<packages>(environment.apiUrl + 'package/' + packages.id, packages);
+  updatePackage(packages: Packages): Observable<Packages> {
+    return this.http.put<Packages>(environment.apiUrl + 'package/' + packages.id, packages);
   }
 
   deletePackage(id: number): Observable<any> {
